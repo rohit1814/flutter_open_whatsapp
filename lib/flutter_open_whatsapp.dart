a5967ea539cade917fa343c1d4f070f1e08d7d8e
jinosh05 <jinoshprabhuraj5@gmail.com>
2021-11-09 11:18:52 +0530

    Null Safety Migrated

--- lib/flutter_open_whatsapp.dart
+++ lib/flutter_open_whatsapp.dart
@@ -9,8 +9,8 @@ class FlutterOpenWhatsapp {
   static const MethodChannel _channel =
       const MethodChannel('flutter_open_whatsapp');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
